4a83f9c015e9a148d1fd1a9e6167a719e7920722
Zach Lindner <zach.lindner@hotmail.com>
2020-02-02 12:23:47 -0500

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# v1
+<h1 align='center'>
+    zachl.ca - v1
+</h1>
+<p align='center'>
+    v1 of <a href='https://zachl.ca' target='_blank' >zachl.ca</a> made with <a href='https://reactjs.org/' target='_blank'>React</a> and <a href=='https://www.typescriptlang.org/'>TypeScript</a>
+</p>
+
+TODO
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,18 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { createGlobalStyle } from 'styled-components';
 
-render(<div></div>, document.getElementById('root'));
+const GlobalStyle = createGlobalStyle`
+    body {
+        margin: 0;
+        padding: 0;
+        oveerflow: hidden;
+    }
+`;
+
+render(
+    <div>
+        <GlobalStyle />
+    </div>,
+    document.getElementById('root')
+);
