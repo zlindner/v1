f0d6d2f6575a6b1c834704a604c0ecb57a4800bd
Zach Lindner <zach.lindner@hotmail.com>
2020-02-03 14:37:29 -0500

    about page

---
+++ src/components/sections/about.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Content = styled.section``;
+
+const About = () => {
+    return <Content id='#about'></Content>;
+};
+
+export default About;
